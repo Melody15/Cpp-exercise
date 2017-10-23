3b49094efef3a7f1d824aa5a00606584f325fb5e
Melody15 <zhuai384177509@qq.com>
2017-10-23 22:48:48 +0800

    Textbook code

---
+++ ex9-vdestructor.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+class felid{
+public:
+    felid() { cout << "felid" << endl; }
+    ~felid() { cout << "~felid" << endl; }
+};
+
+class tiger: public felid{
+public:
+    tiger() { cout << "tiger" << endl; }
+    ~tiger() { cout << "~tiger" << endl; }
+};
+
+int main(int argc, char const *argv[]){
+    felid *p = new tiger;
+    delete p;
+    return 0;
+}
